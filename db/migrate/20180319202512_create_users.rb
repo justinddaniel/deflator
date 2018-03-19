9391ba282d9c54b185a2a3b746c1032ca72dbdc8
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 16:35:03 -0400

    Rails: users table

--- db/migrate/20180319202512_create_users.rb
+++ db/migrate/20180319202512_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+    	t.string :name
+    	t.float :weight
+    	t.float :height
+    	t.float :bmi
+    	t.float :weight_goal
+    	t.float :weekly_target
+    	t.integer :calorie_allot
     end
   end
 end
