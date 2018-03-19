fcec201db1d03de6480a731bcdb9696da11e83d3
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 16:46:22 -0400

    Rails: food table

--- db/migrate/20180319202712_create_foods.rb
+++ db/migrate/20180319202712_create_foods.rb
@@ -1,6 +1,12 @@
 class CreateFoods < ActiveRecord::Migration[5.1]
   def change
     create_table :foods do |t|
+    	t.string :name
+    	t.float :calories_per_serv
+    	t.float :servings
+    	t.integer :nutrition_rating
+    	t.integer :fulfillment_rating
+    	t.integer :day_id
     end
   end
 end
