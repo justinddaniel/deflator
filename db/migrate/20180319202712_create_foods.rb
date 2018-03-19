62de46116210f434448e8e4def9a83965254d69d
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 17:01:35 -0400

    Rails editing tables

--- db/migrate/20180319202712_create_foods.rb
+++ db/migrate/20180319202712_create_foods.rb
@@ -6,7 +6,6 @@ class CreateFoods < ActiveRecord::Migration[5.1]
     	t.float :servings
     	t.integer :nutrition_rating
     	t.integer :fulfillment_rating
-    	t.integer :day_id
     end
   end
 end
