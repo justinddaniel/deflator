1372dc2670b0aa7ee8e58dfb6bd267a1cbd60d95
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 16:55:29 -0400

    Rails: exercises table

--- db/migrate/20180319202732_create_exercises.rb
+++ db/migrate/20180319202732_create_exercises.rb
@@ -1,6 +1,9 @@
 class CreateExercises < ActiveRecord::Migration[5.1]
   def change
     create_table :exercises do |t|
+    	t.string :name
+    	t.float :calories_bpm
+    	t.integer :fun_rating
     end
   end
 end
