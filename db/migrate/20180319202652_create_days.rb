76e92c896cc55785a5b8c840723d3ccbee984552
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 16:43:32 -0400

    Rails: days table

--- db/migrate/20180319202652_create_days.rb
+++ db/migrate/20180319202652_create_days.rb
@@ -1,6 +1,13 @@
 class CreateDays < ActiveRecord::Migration[5.1]
   def change
     create_table :days do |t|
+    	t.string :title
+    	t.date :date
+    	t.integer :journal_id
+    	t.integer :calorie_allot
+    	t.integer :calories_consumed
+    	t.integer :calories_burned
+    	t.integer :net_calories
     end
   end
 end
