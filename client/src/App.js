4bbabf7a2984bb78f4d199e56ac4b3199b86491e
Justin Daniel <justinddaniel@gmail.com>
2018-03-19 23:28:57 -0400

    React initial css styling complete

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      	<h1>Deflator diet and exercise tracking app</h1>
       	<div className="navbar">
       		<p>navbar</p>
       	</div>
