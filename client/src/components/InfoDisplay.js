8040b24862ef84a0317e21ef11588da70190913b
Justin Daniel <justinddaniel@gmail.com>
2018-03-21 19:35:29 -0400

    React minor edits to allow rendering

--- client/src/components/InfoDisplay.js
+++ client/src/components/InfoDisplay.js
@@ -7,12 +7,12 @@ const User = ({ user }) => {
       {user ?
         <div>
           <h3>{user.name}</h3>
-          <p>Weight: {user.weight}</p><br><br>
-          <p>Height: {user.height}</p><br><br>
-          <p>BMI: {user.bmi}</p><br><br>
-          <p>Weekly Target: {user.weekly_target}</p><br><br>
-          <p>Weight Goal: {user.weight_goal}</p><br><br>
-          <p>Calorie Allotment: {user.calorie_allot}</p><br><br>
+          <p>Weight: {user.weight}</p><br></br>
+          <p>Height: {user.height}</p><br></br>
+          <p>BMI: {user.bmi}</p><br></br>
+          <p>Weekly Target: {user.weekly_target}</p><br></br>
+          <p>Weight Goal: {user.weight_goal}</p><br></br>
+          <p>Calorie Allotment: {user.calorie_allot}</p><br></br>
         </div>
         :
         <div>
