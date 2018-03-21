5aed44b5578d89d7d8a8e6a457589165590e9110
Justin Daniel <justinddaniel@gmail.com>
2018-03-21 18:19:24 -0400

    Rewiring infodisplay for redux

--- client/src/components/InfoDisplay.js
+++ client/src/components/InfoDisplay.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Users = ({ users }) => {
-  const renderInfo = users.map(user => 
-    <p key={user.id}>{user.name}</p>
-  )
-
+const User = ({ user }) => {
   return (
     <div>
-      {renderInfo}
+      {user ?
+        <div>
+          <h3>{user.name}</h3>
+          <p>Weight: {user.weight}</p><br><br>
+          <p>Height: {user.height}</p><br><br>
+          <p>BMI: {user.bmi}</p><br><br>
+          <p>Weekly Target: {user.weekly_target}</p><br><br>
+          <p>Weight Goal: {user.weight_goal}</p><br><br>
+          <p>Calorie Allotment: {user.calorie_allot}</p><br><br>
+        </div>
+        :
+        <div>
+          ...loading please wait 
+        </div>
+      }
     </div>
-  )
+  );
 }
 
-export default Users;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.users.find(user => user.id == ownProps.match.params.userId)
+  }
+}
+
+export default connect(mapStateToProps)(User);
