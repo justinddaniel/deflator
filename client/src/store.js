0b7efcd7f4bef0e619ae405e712d68ba2580732c
Justin Daniel <justinddaniel@gmail.com>
2018-03-20 22:17:24 -0400

    React wired store.js to import reducers. Reducers will have own folder and consist of users and userFormData

--- client/src/store.js
+++ client/src/store.js
@@ -4,3 +4,18 @@ import {
   combineReducers
 } from 'redux';
 import thunk from 'redux-thunk';
+
+import users from './reducers/users';
+import userFormData from './reducers/userFormData';
+
+const reducers = combineReducers({
+  users,
+  userFormData
+});
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
